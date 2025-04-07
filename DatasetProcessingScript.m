9ea7774509a9921c0fc5b4cb40aac4564f7c4cd1
scottkil <scottkilianski@gmail.com>
2025-04-07 14:27:57 -0400

    Adding data processing script

--- DatasetProcessingScript.m
+++ DatasetProcessingScript.m
@@ -1,10 +1,20 @@
-%% 1. Convert to .imgbin
+%% 1 Convert to .imgbin
 filename = ''; % full filepath to object
 dcimgToBin(filename);
 
-%% 2. Image registration and segmentation
-img_file = '/media/scott3X/SI_Data/Sakina Gria x GCaMP_server/20240930/20240930_22800001.imgbin'; 
-brain_img = imread('/media/scottX/Random/SI_017_20230209_BrightnessEnhanced.png');
+%% 2 Image registration and segmentation
+% img_file = '/media/scott3X/SI_Data/Sakina Gria x GCaMP_server/20240930/20240930_22800001.imgbin'; 
+img_file = 'V:\SI_Data\Sakina Gria x GCaMP_server\20240930\20240930_22800001.imgbin';
+% brain_img = imread('/media/scottX/Random/SI_017_20230209_BrightnessEnhanced.png');
 img = imgbinRead(img_file);     % read in the imaging data
 frame1 = img.Data.frames(:,:,1);
-reg_img = imageMorphing(brain_img);
+brain_img = im2uint8(frame1);
+atlas_img = imread('atlas_noOB_binary.png');
+% brain_img = imread('Z:\Random\SI_017_20230209_BrightnessEnhanced.png');
+cph = cpselect(atlas_img,brain_img);
+
+%% 2.1 
+reg_img = imageMorphing(brain_img,atlas_img,fixedPoints,movingPoints);
+
+%% 3. 
+imshow(reg_img)
